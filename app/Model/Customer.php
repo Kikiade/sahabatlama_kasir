0083eb17a198b1544a076538e0063b8d1ae692b7
koprals <kopralrhp@gmail.com>
2014-05-23 15:29:36 +0700

    customer validate

--- app/Model/Customer.php
+++ app/Model/Customer.php
@@ -1,5 +1,11 @@
 <?php
 	class Customer extends AppModel{
-		
+		public $validate = array(
+			'name' => array(
+				'rule' => 'NotEmpty'
+				),
+				'vehicle_number' => array(
+					'rule' => 'NotEmpty')
+			);
 	}
 ?>
